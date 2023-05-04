a9662c1dddeeb9ba9f17e12491c4e80532e9650d
witehound <netvid00@gmail.com>
2023-05-04 17:20:38 +0100

    debuged new react-youtube component

--- src/components/ProblemsTable.tsx
+++ src/components/ProblemsTable.tsx
@@ -53,11 +53,11 @@ export default function ProblemsTable() {
         })}
       </tbody>
       <tfoot className="fixed top-0 left-0 h-screen w-screen flex items-center justify-center ">
-        {/* <div className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute"></div>
-        <div className="w-full z-50 h-full px-6 relative max-w-4xl">
-          <div className="w-full h-full flex items-center justify-center relative">
-            <div className="w-full relative"> */}
-        {/* <IoClose
+        <tr className="bg-black z-10 opacity-70 top-0 left-0 w-screen h-screen absolute" />
+        <tr className="w-full z-50 h-full px-6 relative max-w-4xl">
+          <tr className="w-full h-full flex items-center justify-center relative">
+            <tr className="w-full relative">
+              <IoClose
                 fontSize={"35"}
                 className="cursor-pointer absolute -top-16 right-0"
               />
@@ -65,10 +65,10 @@ export default function ProblemsTable() {
                 videoId={"xty7fr-k0TU"}
                 loading="lazy"
                 iframeClassName="w-full min-h-[500px]"
-              /> */}
-        {/* </div>
-          </div>
-        </div> */}
+              />
+            </tr>
+          </tr>
+        </tr>
       </tfoot>
     </>
   );
