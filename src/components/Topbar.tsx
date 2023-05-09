86a144e71558f1310d03ca776ab9b569a5a4facb
witehound <netvid00@gmail.com>
2023-05-09 09:20:38 +0100

    added logout button

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -2,6 +2,8 @@ import { firebaseAuth } from "@/firebase/firebase";
 import Image from "next/image";
 import Link from "next/link";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { Button } from ".";
+import { FiLogOut } from "react-icons/fi";
 
 export default function Topbar() {
   const [user] = useAuthState(firebaseAuth);
@@ -26,16 +28,23 @@ export default function Topbar() {
             </a>
           </div>
           {user ? (
-            <div className=" relative cursor-pointer group">
-              <Image src="/avatar.png" alt="avater" width={30} height={30} />
-              <div
-                className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
+            <>
+              <div className=" relative cursor-pointer group">
+                <Image src="/avatar.png" alt="avater" width={30} height={30} />
+                <div
+                  className="absolute top-10 left-2/4 -translate-x-2/4  mx-auto bg-dark-layer-1 text-brand-orange p-2 rounded shadow-lg 
 								z-40 group-hover:scale-100 scale-0 
 								transition-all duration-300 ease-in-out"
-              >
-                <p>{user.email}</p>
+                >
+                  <p>{user.email}</p>
+                </div>
               </div>
-            </div>
+              <Button
+                text={<FiLogOut />}
+                style="bg-dark-fill-3 py-1.5 px-3 cursor-pointer rounded text-brand-orange"
+                type="button"
+              />
+            </>
           ) : (
             <Link href="/auth">
               <button className="bg-dark-fill-3 py-1 px-2 cursor-pointer rounded ">
