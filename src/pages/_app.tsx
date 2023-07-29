400ac439cda56ad69c25531d73976742b3de5521
witehound <netvid00@gmail.com>
2023-07-29 13:46:32 +0100

    added head tag in the app file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>DSArena</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+
+        <meta
+          name="description"
+          content="Web application that contains DSA problems and solutions"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
