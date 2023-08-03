70387df3f1808455447544c8125af84416cd78f7
witehound <netvid00@gmail.com>
2023-08-03 14:14:51 +0100

    completed youtube modal functionality

--- src/components/ProblemsTable.tsx
+++ src/components/ProblemsTable.tsx
@@ -2,8 +2,14 @@ import { problems } from "@/data/mock.problems";
 import Link from "next/link";
 import { BsCheckCircle } from "react-icons/bs";
 import { AiFillYoutube } from "react-icons/ai";
+import { useSetRecoilState } from "recoil";
+import { youtubeAtomModalState } from "@/atoms/youtubeModal.atom";
 
 export default function ProblemsTable() {
+  const updateYoutubeModalState = useSetRecoilState(youtubeAtomModalState);
+  const handleOpenYoutubeModal = (videoId: string | undefined) => {
+    updateYoutubeModalState((prev) => ({ ...prev, isOpen: true, videoId }));
+  };
   return (
     <>
       {" "}
@@ -42,6 +48,9 @@ export default function ProblemsTable() {
                   <AiFillYoutube
                     fontSize={"28"}
                     className={"cursor-pointer hover:text-red-600"}
+                    onClick={() => {
+                      handleOpenYoutubeModal(problem.videoId);
+                    }}
                   />
                 ) : (
                   <p className=" text-gray-400">Coming soon</p>
